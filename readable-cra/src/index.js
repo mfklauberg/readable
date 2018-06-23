add038eab3a77b4fcaee4e827672a1ae07f4a4de
Matheus Klauberg <mfklauberg@gmail.com>
2018-06-23 17:27:26 -0300

    feat: add redux-sagas

--- readable-cra/src/index.js
+++ readable-cra/src/index.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const noop = () => {};
+import rootSaga from './sagas';
+import reducer from './reducers';
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //eslint-disable-line
 
-const store = createStore(noop, composeEnhancers());
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware)),
+);
+
+sagaMiddleware.run(rootSaga);
 
 const Readable = () => (
   <Provider store={store}>
